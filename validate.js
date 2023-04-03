77650bd497d7237942955efd402635b523bf8df7
eldeson_nascimento <eldesonnascimento@gmail.com>
2023-04-03 15:43:29 -0300

    validacao index

---
+++ validate.js
@@ -0,0 +1,10 @@
+import{validaNome} from "./js/valida-nome.js"
+import{validaEmail} from "./js/valida-email.js"
+import{validaAssunto} from "./js/valida-assunto.js"
+import{validaMensagem} from "./js/valida-mensagem.js"
+
+
+validaNome();
+validaEmail();
+validaAssunto();
+validaMensagem();
